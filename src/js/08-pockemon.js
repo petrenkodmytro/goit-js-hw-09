2a00d8d634f9fbfaef781531c3ef60b8cd6b8aa7
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-03-05 21:13:36 +0200

    add pokemon card

---
+++ src/js/08-pockemon.js
@@ -0,0 +1,49 @@
+const seachFormRef = document.querySelector('.js-seach-form');
+const cardRef = document.querySelector('.js-card-container');
+
+seachFormRef.addEventListener('submit', onSeachPokemonSubmit);
+
+function onSeachPokemonSubmit(e) {
+  e.preventDefault();
+  cardRef.innerHTML = '';
+  const seachValue = seachFormRef.elements.query.value;
+  fetchPokemon(seachValue)
+    .then(renderPokemonCard)
+    .catch(error => {
+      console.log(error);
+      alert('Упс, трапилась біда!');
+    })
+    .finally(() => {
+      seachFormRef.reset();
+    });
+}
+
+// функция принесла и распарсила промис с данными
+function fetchPokemon(pokemonId) {
+  return fetch(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
+    response => {
+      return response.json();
+    }
+  );
+}
+
+// функция создания разметки
+function renderPokemonCard(pokemon) {
+  const markup = `<img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+  <p>Name: <b>${pokemon.name}</b></p>
+  <p>Weight: <b>${pokemon.weight}</b> cm</p>
+  <p>Height: <b>${pokemon.height}</b> kg</p>`;
+
+  cardRef.insertAdjacentHTML('beforeend', markup);
+}
+
+// fetch(`http://pokeapi.co/api/v2/pokemon/101`)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(pokemon => {
+//     renderPokemonCard(pokemon);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
