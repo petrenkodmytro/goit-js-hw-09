f2356a192703d51a46f4377c74a499138e3dc6aa
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-03-07 21:31:51 +0200

    change api

--- src/js/09-seach-news.js
+++ src/js/09-seach-news.js
@@ -1,4 +1,68 @@
-const YOUR_API_KEY = 'ea323ce12d5643248c09e504b34a1936';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '504d5aef12msh28872f0ea14deb5p1f3bbcjsnd108e0bdaaa0',
+    'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
+  },
+};
+
+function fetchData(query) {
+  return fetch(
+    `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=${query}&pageNumber=1&pageSize=20&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null`,
+    options
+  ).then(response => response.json());
+}
+
+import Notiflix from 'notiflix';
+const seachFormRef = document.querySelector('.js-seach-form');
+const newsWrapperRef = document.querySelector('.js-news-container');
+
+seachFormRef.addEventListener('submit', onSeachNews);
+
+function onSeachNews(e) {
+  e.preventDefault();
+
+  const seachValue = seachFormRef.elements.news.value.trim();
+
+  fetchData(seachValue)
+    .then(({ value }) => {
+      if (value.length === 0) {
+        throw new Error('No data');
+      }
+      renderNewsCard(value);
+      // console.log(articles);
+    })
+    .catch(error => {
+      newsWrapperRef.innerHTML = '';
+      Notiflix.Report.failure('Attention!!!', 'Not found anything', 'Ok');
+      console.error(error);
+    })
+    .finally(() => {
+      seachFormRef.reset();
+    });
+}
+
+// функция создания разметки
+function cardMarkup({ name, provider, description, ampUrl, image }) {
+  return `<div class="article-card">
+  <img class="article-img" src="${image.url}" alt="" />
+      <h3 class="article-title">${name}</h3>
+      <h4 class="article-author">${provider.name || 'Anonymus'}</h4>
+      <p class="article-text">${description}</p>
+      <a class="article-link" href="${ampUrl}">Read more</a>
+  </div>`;
+}
+
+// функция рендера страницы
+function renderNewsCard(arrayNews) {
+  const listNewsMarkup = arrayNews.reduce((acc, article) => {
+    return cardMarkup(article) + acc;
+  }, '');
+
+  newsWrapperRef.innerHTML = listNewsMarkup;
+}
+
+// const YOUR_API_KEY = 'ea323ce12d5643248c09e504b34a1936';
 // import { NewsAPI } from 'newsapi';
 // const NewsAPI = require('newsapi');
 // // const newsapi = new NewsAPI(YOUR_API_KEY);
@@ -14,63 +78,13 @@ const YOUR_API_KEY = 'ea323ce12d5643248c09e504b34a1936';
 //     console.log(response);
 //   });
 
-import Notiflix from 'notiflix';
-const seachFormRef = document.querySelector('.js-seach-form');
-const newsWrapperRef = document.querySelector('.js-news-container');
-
-seachFormRef.addEventListener('submit', onSeachNews);
-
-function onSeachNews(e) {
-  e.preventDefault();
-
-  const seachValue = seachFormRef.elements.news.value.trim();
-
-  fetchData(seachValue)
-    .then(({ articles }) => {
-      if (articles.length === 0) {
-        throw new Error('No data');
-      }
-      renderNewsCard(articles);
-      // console.log(articles);
-    })
-    .catch(error => {
-      newsWrapperRef.innerHTML = '';
-      Notiflix.Report.failure('Attention!!!', 'Not found anything', 'Ok');
-      console.error(error);
-    })
-    .finally(() => {
-      seachFormRef.reset();
-    });
-}
-
 // функция принесла и распарсила промис с данными
 // const options = {
 //   headers: {
 //     'X-Api-Key': 'ea323ce12d5643248c09e504b34a1936',
 //   },
 // };
-
-function fetchData(query) {
-  const URL = `https://newsapi.org/v2/everything?q=${query}&apiKey=ea323ce12d5643248c09e504b34a1936`;
-  return fetch(URL).then(response => response.json());
-}
-
-// функция создания разметки
-function cardMarkup({ author, title, description, url, urlToImage }) {
-  return `<div class="article-card">
-  <img class="article-img" src="${urlToImage}" alt="" />
-      <h3 class="article-title">${title}</h3>
-      <h4 class="article-author">${author || 'Anonymus'}</h4>
-      <p class="article-text">${description}</p>
-      <a class="article-link" href="${url}">Read more</a>
-  </div>`;
-}
-
-// функция рендера страницы
-function renderNewsCard(arrayNews) {
-  const listNewsMarkup = arrayNews.reduce((acc, article) => {
-    return cardMarkup(article) + acc;
-  }, '');
-
-  newsWrapperRef.innerHTML = listNewsMarkup;
-}
+// function fetchData(query) {
+//   const URL = `https://newsapi.org/v2/everything?q=${query}&apiKey=ea323ce12d5643248c09e504b34a1936`;
+//   return fetch(URL).then(response => response.json());
+// }
